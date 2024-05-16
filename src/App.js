e52a450f57c0b9fd640e47aeed30a1c522493e8b
studentcredit <prakashkumarpandey126@gmail.com>
2024-05-16 17:44:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
-import Home from "./Partnerscreen/partnerScreen";
+// App.js
+import React, { useState, useEffect } from 'react';
+import Home from './Partnerscreen/partnerScreen';
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import config from './Config/config.json';
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  )
+    const [showData, setShowData] = useState(false);
+    const [curTime, setCurTime] = useState(new Date());
+
+    useEffect(()=>{
+      const interval = setInterval(()=>{
+        setCurTime(new Date())
+      },1000)
+      return () =>{
+        clearInterval(interval)
+      }
+    }, [])
+
+    useEffect(()=>{
+      const scheduleTime = new Date(config.seduleDate + 'T' + config.seduleTime);
+      const showDuration = config.showDuration || 5; // default duration is 5 minutes
+      const endTime = new Date(scheduleTime.getTime() + showDuration * 60000);
+      setShowData(curTime >= scheduleTime && curTime < endTime);
+    }, [curTime])
+
+    return (
+     <>
+         <p>Current Time: {curTime.toLocaleString()}</p>
+         {showData && <Home />}
+     </>
+    );
 }
 
 export default App;
