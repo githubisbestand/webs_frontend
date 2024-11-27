73a246b10748434eb9a4983f0057118965c1a9b1
studentcredit <prakashkumarpandey126@gmail.com>
2024-11-27 14:53:10 +0530

    changes commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './react-crud/store/store';
 import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';  
 import 'react-toastify/dist/ReactToastify.css';
 
 
